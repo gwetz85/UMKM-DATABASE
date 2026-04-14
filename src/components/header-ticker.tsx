21a66c611f7252ef9a0b7b15b4a80c07d5c7b744
Developer <developer@example.com>
2026-04-14 19:53:09 +0700

    text

--- src/components/header-ticker.tsx
+++ src/components/header-ticker.tsx
@@ -5,7 +5,7 @@ import { Info } from 'lucide-react';
 
 export function HeaderTicker() {
   const [mounted, setMounted] = useState(false);
-  const message = "Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 5.2. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih.";
+  const message = "Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 5.2. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih. --- ";
 
   useEffect(() => {
     setMounted(true);
@@ -23,10 +23,12 @@ export function HeaderTicker() {
       </div>
       <div className="relative flex-1 h-full flex items-center overflow-hidden bg-white/50 backdrop-blur-sm">
         <div className="animate-marquee whitespace-nowrap text-primary font-bold text-xs py-1 flex gap-8 items-center">
-          <span className="inline-block px-4">{message}</span>
-          <span className="inline-block px-4">{message}</span>
-          <span className="inline-block px-4">{message}</span>
-          <span className="inline-block px-4">{message}</span>
+          <span className="inline-block px-4">
+            Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 5.2. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih.
+            Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 5.2. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih.
+            Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 7.5. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih.
+            Selamat datang di Aplikasi SIMPU (Sistem Informasi Manajemen Pelaku Usaha) versi 5.2. Aplikasi ini dirancang untuk mempermudah dalam pengecekkan dan penginputan data. Kritik dan saran sangat diperlukan. Kirimkan pesan ke email: agussuriyadipunya@gmail.com atau Whatsapp: 0817319885. Terima kasih. ---- 
+          </span>
         </div>
       </div>
     </div>
